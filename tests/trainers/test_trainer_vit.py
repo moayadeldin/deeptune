8b9bf20ef3e15a42776e2dbfb28cff4c50242beb
moayad <moayadeldin.tamer@ejust.edu.eg>
2025-04-21 00:31:43 -0300

    adding test cases for trainers

---
+++ tests/trainers/test_trainer_vit.py
@@ -0,0 +1,53 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from src.vision.vit import adjustedViT
+from src.vision.vit_peft import adjustedViTPeft
+from trainers.vision.trainer import Trainer
+
+def create_dummy_loader(num_classes=3):
+    x = torch.randn(8, 3, 224, 224)
+    y = torch.randint(0, num_classes, (8,))
+    dataset = TensorDataset(x, y)
+    return DataLoader(dataset, batch_size=2)
+
+def test_trainer_step_classification():
+    model = adjustedViT(num_classes=3, vit_version="dummy", added_layers=1, task_type="cls")
+    train_loader = create_dummy_loader()
+    val_loader = create_dummy_loader()
+    
+    trainer = Trainer(
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        learning_rate=0.001,
+        mode="cls",
+        num_epochs=1,
+        output_dir=None
+    )
+
+    trainer.train()
+    val_loss, val_acc = trainer.validate()
+    
+    assert isinstance(val_loss, float)
+    assert 0 <= val_acc <= 1
+
+def test_trainer_peft_step_classification():
+    model = adjustedViTPeft(num_classes=3, vit_version="dummy", added_layers=1, task_type="cls")
+    train_loader = create_dummy_loader()
+    val_loader = create_dummy_loader()
+    
+    trainer = Trainer(
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        learning_rate=0.001,
+        mode="cls",
+        num_epochs=1,
+        output_dir=None
+    )
+
+    trainer.train()
+    val_loss, val_acc = trainer.validate()
+    
+    assert isinstance(val_loss, float)
+    assert 0 <= val_acc <= 1
